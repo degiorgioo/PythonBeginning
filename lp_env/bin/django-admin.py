7750bd34bfe200a2e0e681d714f1e5bddddd27ca
degiorgioo <dave_degiorgio@hotmail.com>
2017-02-08 12:42:24 +0100

    lp_env -> learning python environment

---
+++ lp_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dada/Desktop/HSR/workspaces/pyCharm/Beginning/lp_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
