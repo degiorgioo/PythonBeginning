a3862d5c19bde557f21661ae8518a2c35c5504c2
degiorgioo <dave_degiorgio@hotmail.com>
2017-02-11 12:02:39 +0100

    setup.py completed

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 import io
 from setuptools import setup, find_packages
 
-setup()
+setup(
+    name='PythonBeginning',
+    version='1.0',
+    description='Beginning of a Pyhton Ära.',
+    author='Davide De Giorgio'
+    )
