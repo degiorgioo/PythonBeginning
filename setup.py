b4799f5237c205416c684fc2cc456bc9c219b040
degiorgioo <dave_degiorgio@hotmail.com>
2017-02-22 12:21:50 +0100

    to run build

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name='PythonBeginning',
     version='1.0',
-    description='Beginning of a Pyhton Ära.',
+    description='Beginning of a Pyhton Era.',
     author='Davide De Giorgio'
     )
