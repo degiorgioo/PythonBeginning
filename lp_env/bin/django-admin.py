8176ade2ae38660306ce4bcb74be4502c700af2d
Davide De Giorgio <dave_degiorgio@hotmail.com>
2017-02-25 18:02:24 +0100

    python2.7 deleted from travis

--- lp_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/dada/Desktop/HSR/workspaces/pyCharm/Beginning/lp_env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
