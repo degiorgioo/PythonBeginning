909cdc8351d79d65b03a4d89d134691a19a80530
Davide De Giorgio <dave_degiorgio@hotmail.com>
2017-02-27 15:40:38 +0100

    .landscape.yaml

--- setup.py
+++ setup.py
@@ -5,5 +5,13 @@ setup(
     name='PythonBeginning',
     version='1.0',
     description='Beginning of a Pyhton Era.',
-    author='Davide De Giorgio'
+    author='Davide De Giorgio',
+    classifiers=[
+              'Programming Language :: Python :: 2',
+              'Programming Language :: Python :: 2.6',
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 3.3',
+              'Programming Language :: Python :: 3.4',
+          ]
     )
